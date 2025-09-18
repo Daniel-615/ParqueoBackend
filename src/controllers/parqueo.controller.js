fd73c85874955762b4b96ad5d80c72c6b778d202
Daniel-615 <suyanangel615@gmail.com>
2025-09-18 16:43:01 -0600

    cambiada la fecha a iso string

--- src/controllers/parqueo.controller.js
+++ src/controllers/parqueo.controller.js
@@ -13,10 +13,7 @@ class ParqueoController {
       nombre: parqueo.nombre,
       ocupado: !!parqueo.ocupado,
       activo: !!parqueo.activo,
-      updatedAt: parqueo.updatedAt ?new Date(parqueo.updatedAt).toLocaleString("es-Gt",{
-        timeZone: "America/Guatemala",
-      })
-      :null,
+      updatedAt: parqueo.updatedAt ?new Date(parqueo.updatedAt).toISOString(): null
     };
     io.of("/parqueos").to("parqueos:all").emit("parqueo_updated", payload);
     io.of("/parqueos").to(`parqueo:${parqueo.id}`).emit("parqueo_updated", payload);
